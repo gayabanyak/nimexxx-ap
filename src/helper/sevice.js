47f19b9cd329b147561ce8a19e80b436b64ed53a
gayaabanyak <gayaabanyak7@gmail.com>
2025-07-19 05:08:53 +0700

    fix: update axios headers to prevent 403 error

--- src/helper/sevice.js
+++ src/helper/sevice.js
@@ -4,12 +4,13 @@ const Service = {
     fetchService: async (url, res) => {
         try {
             const response = await axios.get(url, {
-                timeout: 7000, // ⏱️ timeout maksimal
+                timeout: 7000,
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Node.js Server)'
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36',
+                    'Referer': url
                 }
             });
-            return response; // langsung kembalikan response
+            return response;
         } catch (error) {
             console.error("❌ Error fetching URL:", url);
             res.status(500).json({
