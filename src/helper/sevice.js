3fe86d04993894681859976e58a487981da2e16a
gayaabanyak <gayaabanyak7@gmail.com>
2025-07-19 05:23:17 +0700

    f

--- src/helper/sevice.js
+++ src/helper/sevice.js
@@ -6,11 +6,13 @@ const Service = {
       const response = await axios.get(url, {
         timeout: 7000,
         headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-          'Referer': url,
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-          'Accept-Language': 'en-US,en;q=0.9'
-        }
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+  'Referer': 'https://otakudesu.cloud/',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+  'Accept-Language': 'en-US,en;q=0.9',
+  'Cache-Control': 'no-cache',
+    }
+
       });
       return response;
     } catch (error) {
